447cbb8fa6c717ee500c7de31817c17edceb1dbf
evenyosua18 <evenyosua18@gmail.com>
2025-05-02 01:18:51 +0700

    sort router based on group name

--- router/registry.go
+++ router/registry.go
@@ -1,5 +1,7 @@
 package router
 
+import "sort"
+
 var (
 	groupRegistry = make(map[string]RouteFunc)
 )
@@ -21,11 +23,24 @@ func RegisterRouteByGroup(group string, routeFns []RouteFunc) {
 }
 
 func RegisterRoutes(r IRouter) {
-	for group, fn := range groupRegistry {
-		if group == "public" {
-			fn(r.Group("/"))
-		} else {
-			fn(r.Group("/" + group))
+	// always register public routes first if available
+	if fn, ok := groupRegistry["public"]; ok {
+		fn(r.Group("/"))
+	}
+
+	// map groups, except public
+	var groups []string
+	for g := range groupRegistry {
+		if g != "public" {
+			groups = append(groups, g)
 		}
+	}
+
+	// sort group
+	sort.Strings(groups)
+
+	// loop based on sorted group name
+	for _, groupName := range groups {
+		groupRegistry[groupName](r.Group("/" + groupName))
 	}
 }
