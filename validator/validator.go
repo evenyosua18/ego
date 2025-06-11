cd495b8c807d3ffecfbbf694e1fd36d17e5f7cec
evenyosua18 <evenyosua18@gmail.com>
2025-06-11 22:05:16 +0700

    add validator

---
+++ validator/validator.go
@@ -0,0 +1,34 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/evenyosua18/ego/code"
+	"github.com/go-playground/validator/v10"
+	"sync"
+)
+
+var (
+	val  *validator.Validate
+	once sync.Once
+)
+
+func Validate(request any) error {
+	// initiate
+	once.Do(func() {
+		val = validator.New()
+	})
+
+	// validate
+	err := val.Struct(request)
+
+	// manage error
+	if err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			return code.Get(code.BadRequestError).
+				SetErrorMessage(fmt.Sprintf("field '%s' failed on the tag %s", e.Field(), e.Tag())).
+				SetMessage(fmt.Sprintf("please check field '%s' again", e.Field()))
+		}
+	}
+
+	return nil
+}
